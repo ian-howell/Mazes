2301ad5493f8e4448b3553bb94d92828c7707a23
Ian Howell <ian.howell0@gmail.com>
2016-08-05 22:56:04 -0500

    usage function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <ncurses.h>
 #include <cstdlib>
 
+void usage(const char* prgname);
+
 int main(int argc, char** argv)
 {
     Maze *maze = NULL;
@@ -25,4 +27,22 @@ int main(int argc, char** argv)
     delete maze;
 
     return 0;
+}
+
+void usage(const char* prgname)
+{
+    printf("Usage: %s [OPTION]...\n", prgname);
+    printf("Example: %s -r 10 -c 15 -a\n", prgname);
+    printf("%-20s", "  -r NUM");
+    printf("%-30s\n", "Maze will have NUM rows");
+    printf("%-20s", "  -c NUM");
+    printf("%-30s\n", "Maze will have NUM columnsrows");
+    printf("%-20s", "  -a");
+    printf("%-30s\n", "Animate maze with ncurses");
+    printf("%-20s", "  -u");
+    printf("%-30s\n", "Print resulting unsolved maze");
+    printf("%-20s", "  -s");
+    printf("%-30s\n", "Print resulting solved maze");
+    printf("%-20s", "  -h");
+    printf("%-30s\n", "Show this help message");
 }
