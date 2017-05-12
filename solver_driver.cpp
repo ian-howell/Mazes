c9ec1cc33f961b49ccdc6d8a757fb1f1277e1132
Ian Howell <ian.howell0@gmail.com>
2017-05-11 21:38:27 -0500

    Initial solver_driver

--- solver_driver.cpp
+++ solver_driver.cpp
@@ -9,59 +9,39 @@
 #include "player.h"
 #include "generator.h"
 
-void usage(const char* prgname);
-
 int main(int argc, char** argv)
 {
-  int myarg;
-  int rows = 10;
-  int cols = 10;
-  int flags;
-
-  // Get the screen dimensions
-  initscr();
-  getmaxyx(stdscr, rows, cols);
-  endwin();
-
-  // Normalize the dimensions so there is a border between the edge of
-  // the screen and the maze walls
-  rows -= 2;
-  cols -= 2;
-
   int c;
   int opt_index = 0;
-
-  std::string algorithm = "nothing";
+  char* algorithm = NULL;
+  char* input_file = NULL;
 
   struct option opts[] = {
-    {"rows",         required_argument, 0, 'r'},
-    {"cols",         required_argument, 0, 'c'},
-    {"algorithm",    required_argument, 0, 'a'}
+    {"algorithm",    required_argument, 0, 'a'},
+    {"file",         required_argument, 0, 'f'}
   };
 
   while ((c = getopt_long(argc, argv, "r:c:a:h", opts, &opt_index)) != -1)
   {
     switch (c)
     {
-      case 'r':
-        rows = atoi(optarg);
-        break;
-      case 'c':
-        cols = atoi(optarg);
-        break;
       case 'a':
         algorithm = optarg;
         break;
-      case 'h':
-        usage(argv[0]);
-        return 0;
+      case 'f':
+        input_file = optarg;
+        break;
     }
   }
 
-  Maze* maze = new Maze(rows, cols);
-  Generator* generator = new Generator(maze);
-  generator->create_maze(false);
-  maze->print();
+  if (!algorithm || !input_file)
+  {
+    fprintf(stderr, "Both an algorithm and an input file are required\n");
+    return 0;
+  }
+
+  Maze* maze = new Maze(input_file);
+  maze->print("out.txt");
 
   /* Solver* solver = new Solver(maze, ANIMATE_SOLVE); */
   /* Player* player = new Player(maze, 0, 0); */
@@ -120,33 +100,9 @@ int main(int argc, char** argv)
 
   /* getch(); */
 
-  endwin();
-
-  /* if (PRINT_SOLVED) */
-  /*     maze->print(); */
-
   delete maze;
-  delete generator;
   /* delete solver; */
   /* delete player; */
 
   return 0;
-}
-
-void usage(const char* prgname)
-{
-  printf("Usage: %s [OPTION]...\n", prgname);
-  printf("Example: %s -r 10 -c 15 -usa\n", prgname);
-  printf("%-20s", "  -r NUM");
-  printf("%-30s\n", "Maze will have NUM rows");
-  printf("%-20s", "  -c NUM");
-  printf("%-30s\n", "Maze will have NUM columnsrows");
-  printf("%-20s", "  -a");
-  printf("%-30s\n", "Animate maze with ncurses");
-  printf("%-20s", "  -u");
-  printf("%-30s\n", "Print resulting unsolved maze");
-  printf("%-20s", "  -s");
-  printf("%-30s\n", "Print resulting solved maze");
-  printf("%-20s", "  -h");
-  printf("%-30s\n", "Show this help message");
 }
