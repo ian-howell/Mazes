465f631f6401037020ad24eb158e6455b7d561b2
Ian Howell <ian.howell0@gmail.com>
2017-05-11 20:20:07 -0500

    Remove the solving junk from the generator

--- generator_driver.cpp
+++ generator_driver.cpp
@@ -9,14 +9,10 @@
 #include "player.h"
 #include "generator.h"
 
-void usage(const char* prgname);
-
 int main(int argc, char** argv)
 {
-  int myarg;
   int rows = 10;
   int cols = 10;
-  int flags;
 
   // Get the screen dimensions
   initscr();
@@ -52,9 +48,6 @@ int main(int argc, char** argv)
       case 'a':
         algorithm = optarg;
         break;
-      case 'h':
-        usage(argv[0]);
-        return 0;
     }
   }
 
@@ -63,90 +56,8 @@ int main(int argc, char** argv)
   generator->create_maze(false);
   maze->print();
 
-  /* Solver* solver = new Solver(maze, ANIMATE_SOLVE); */
-  /* Player* player = new Player(maze, 0, 0); */
-
-  /* if (MAN_SOLVE) */
-  /* { */
-  /*     start_color(); */
-  /*     noecho(); */
-  /*     cbreak(); */
-
-  /*     refresh(); */
-
-  /*     maze->draw(); */
-
-  /*     int c; */
-
-  /*     while (1) */
-  /*     { */
-  /*         c = wgetch(stdscr); */
-  /*         switch (c) */
-  /*         { */
-  /*             case KEY_UP: */
-  /*                 player->move(UP); */
-  /*                 break; */
-  /*             case KEY_DOWN: */
-  /*                 player->move(DOWN); */
-  /*                 break; */
-  /*             case KEY_LEFT: */
-  /*                 player->move(LEFT); */
-  /*                 break; */
-  /*             case KEY_RIGHT: */
-  /*                 player->move(RIGHT); */
-  /*                 break; */
-  /*         } */
-  /*         /1* player->draw(); *1/ */
-  /*         maze->draw(); */
-
-  /*         if (player->game_won) */
-  /*         { */
-  /*             clear(); */
-  /*             mvwprintw(stdscr, cols / 2, rows / 2, "You win!"); */
-  /*             break; */
-  /*         } */
-  /*     } */
-  /* } */
-  /* else if (BACKTRACK_SOLVE) */
-  /* { */
-  /*     maze->draw(); */
-  /*     solver->backtrack(); */
-  /* } */
-  /* else if (BFS_SOLVE) */
-  /* { */
-  /*     maze->draw(); */
-  /*     solver->breadth_first_search(); */
-  /* } */
-
-  /* getch(); */
-
-  endwin();
-
-  /* if (PRINT_SOLVED) */
-  /*     maze->print(); */
-
   delete maze;
   delete generator;
-  /* delete solver; */
-  /* delete player; */
 
   return 0;
-}
-
-void usage(const char* prgname)
-{
-  printf("Usage: %s [OPTION]...\n", prgname);
-  printf("Example: %s -r 10 -c 15 -usa\n", prgname);
-  printf("%-20s", "  -r NUM");
-  printf("%-30s\n", "Maze will have NUM rows");
-  printf("%-20s", "  -c NUM");
-  printf("%-30s\n", "Maze will have NUM columnsrows");
-  printf("%-20s", "  -a");
-  printf("%-30s\n", "Animate maze with ncurses");
-  printf("%-20s", "  -u");
-  printf("%-30s\n", "Print resulting unsolved maze");
-  printf("%-20s", "  -s");
-  printf("%-30s\n", "Print resulting solved maze");
-  printf("%-20s", "  -h");
-  printf("%-30s\n", "Show this help message");
 }
