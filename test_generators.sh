b3178e0e3f712886348f5ac4e51d3a28e1a0c3f6
Ian Howell <ian.howell0@gmail.com>
2017-05-18 11:15:45 -0500

    Update testers

--- test_generators.sh
+++ test_generators.sh
@@ -2,6 +2,5 @@
 
 for i in "prims" "dfs" "kruskal"
 do
-  echo "./generator_driver --algorithm=$i --animate --speed=5 --rows=50 --cols=50 --file=unsolved.txt"
-  ./generator_driver \--algorithm=$i \--animate \--speed=5 \--rows=50 \--cols=50 \--file=unsolved.txt
+  ./generator_driver \--algorithm=$i \--animate \--speed=5 \--rows=35 \--cols=35 \--file=unsolved.txt
 done
--- test_solver.sh
+++ test_solver.sh
@@ -2,6 +2,5 @@
 
 for i in "bt" "bfs" "dfs" "astar"
 do
-  echo "./generator_driver --algorithm=$i --animate --speed=5 --infile=unsolved.txt -o solved.txt"
   ./solver_driver \--algorithm=$i \--animate \--speed=5 \--infile=unsolved.txt \-o solved.txt
 done
